3edfa4036439bb107a2883e750628290cc645657
cyuser <cyuser@d172-126.jaist.ac.jp>
2024-01-14 22:09:22 +0900

    modify config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 const nextConfig = {
   output: 'export',
   images: {
-    unoptimized: true,
-    domains: [
-      'res.cloudinary.com',
-      'avatars.githubusercontent.com',
-      'imgur.com',
-    ],
+    unoptimized: true
   }
 };
 
